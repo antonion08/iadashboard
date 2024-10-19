6ea1789a6bf903f5b2c74cbbe5bbad45d618b40e
Lunathir1 <147435261+Lunathir1@users.noreply.github.com>
2024-10-18 22:11:35 -0400

    1.2

--- templates/funciones_dashboard.js
+++ templates/funciones_dashboard.js
@@ -1,15 +1,3 @@
-function buscarImagen() {
-    const rutaActual = window.location.pathname;
-    const rutaImagen = `./${rutaActual.replace(/^\/+|\/+$/g, '')}/imagen.png`;
-    console.log(rutaImagen);
-  
-    const image = new Image();
-    image.src = rutaImagen;
-  
-    // Agrega un evento de carga para mostrar la imagen
-    image.onload = function() {
-      document.body.appendChild(image);
-    };
 
 const boton = document.getElementById('boton');
               
