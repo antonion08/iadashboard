484d2c87db349642c03c306c9f8c3ac9f62b1d4b
Lunathir1 <147435261+Lunathir1@users.noreply.github.com>
2024-10-18 21:12:28 -0400

    actualizacion1.2
    prueba, para que busque el directorio de una imagen

--- templates/funciones_dashboard.js
+++ templates/funciones_dashboard.js
@@ -1,3 +1,18 @@
-const image = document.createElement('img');
-image.src = window.URL.createObjectURL(new File(['logo.png'], 'logo.jpg', { type: 'image/png' }));
-document.body.appendChild(image);
+function buscarImagen() {
+    const rutaActual = window.location.pathname;
+    const rutaImagen = `./${rutaActual.replace(/^\/+|\/+$/g, '')}/imagen.png`;
+    console.log(rutaImagen);
+  
+    const image = new Image();
+    image.src = rutaImagen;
+  
+    // Agrega un evento de carga para mostrar la imagen
+    image.onload = function() {
+      document.body.appendChild(image);
+    };
+
+const boton = document.getElementById('boton');
+              
+                boton.addEventListener('click', function() {
+                  window.open('materias.html', '_blank');
+                });
