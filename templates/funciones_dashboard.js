2e7e861c883a078c4ac189f0087631979f927c2b
Lunathir1 <147435261+Lunathir1@users.noreply.github.com>
2024-10-19 17:17:01 -0400

    posible
    posible funcion para chatbot

--- templates/funciones_dashboard.js
+++ templates/funciones_dashboard.js
@@ -4,3 +4,14 @@ const boton = document.getElementById('boton');
                 boton.addEventListener('click', function() {
                   window.open('materias.html', '_blank');
                 });
+
+
+                const express = require('express');
+                const app = express();
+                const axios = require('axios');
+                
+                class ChatBot {
+                  constructor(apiUrl) {
+                    this.apiUrl = https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent;
+                    this.tokenDeAutorizacion = AIzaSyC76BoVVmx7svNQQRtRB7sjgMLK4FEkfBU;
+                  }
