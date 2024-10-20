c4ea4cd41b0c0d3addfdcbd4ccadc1250302017a
antonion08 <antonio08062004@gmail.com>
2024-10-19 21:48:50 -0400

    Update verificacion.php

--- templates/verificacion.php
+++ templates/verificacion.php
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 function verifyUser($usuario, $password) {
     global $conn;
 
-    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND password = '$password'";
+    $sql = "SELECT * FROM login WHERE usuario_login = '$usuario' AND contrasena_login = '$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -26,21 +26,22 @@ function verifyUser($usuario, $password) {
 function initiateSession($usuario, $password) {
     global $conn;
 
-    // Generar un token de sesion
+    // Generar un token de sesion y el datetime de su generación
     $token = uniqid();
+    $fechahora = date('Y-m-d H:i:s');
 
     // Insertar el token en la tabla conexiones
-    $sql = "INSERT INTO conexiones (usuario_id, password, token) VALUES ('$usuario', '$password', '$token')";
+    $sql = "INSERT INTO sesiones (fecha_inicio, token) VALUES ('$fechahora', '$token')";
     $conn->query($sql);
 
     return true;
 }
 
 // Función para cerrar sesión
-function closeSession($id) {
+function closeSession($id_sesion) {
     global $conn;
 
     // Eliminar la conexión de la tabla conexiones
-    $sql = "DELETE FROM conexiones WHERE id = '$id'";
+    $sql = "DELETE FROM sesiones WHERE id_sesion = '$id_sesion'";
     $conn->query($sql);
 }
