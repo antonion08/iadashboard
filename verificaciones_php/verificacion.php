baf6373197c650d5928dd43b4f0456855ccd8915
Lunathir1 <147435261+Lunathir1@users.noreply.github.com>
2024-10-23 14:15:15 -0400

    version preliminar
    organizacion de todos los archivos

---
+++ verificaciones_php/verificacion.php
@@ -0,0 +1,54 @@
+<?php
+
+// Conexión a la base de datos
+$servername = "sql3.freesqldatabase.com";
+$username = "sql3738152";
+$password = "PnV6EPU17z";
+$dbname = "sql3738152";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Comprobar la conexión
+if (!$conn) {
+    die("Error al conectar: " . mysqli_connect_error());
+  }
+  
+  // Función para autenticar
+  function autenticar($username, $contrasena, $estatus) {
+    global $conn;
+    $query = "SELECT * FROM usuarios WHERE username = '$username' AND contraseña = '$contrasena' AND estatus = ?;";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, 'i', $estatus);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+  
+    if ($result->num_rows > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  // Verificar la autenticación
+  $usuario = $_POST['username'];
+  $contrasena = $_POST['contrasena'];
+  $estatus = $_POST['estatus'];
+  
+  if (autenticar($usuario, $contrasena, $estatus)) {
+    // Si el estatus es 1, envíame a la URL 1
+    if ($estatus == 1) {
+      header('Location: http://example.com/url1');
+      exit;
+    }
+    // Si el estatus es 2, envíame a la URL 2
+    elseif ($estatus == 2) {
+      header('Location: http://example.com/url2');
+      exit;
+    } else {
+      echo "No has sido autenticado";
+    }
+  } else {
+    echo "Error al autenticar";
+  }
+  
+  ?>
