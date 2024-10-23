693abad43b95a99d799a40f2a016523f23075272
antonion08 <antonio08062004@gmail.com>
2024-10-23 06:41:58 -0400

    Implementación chatbot
    todavía no responde bien

--- templates/funciones_dashboard.js
+++ templates/funciones_dashboard.js
@@ -2,9 +2,9 @@ const modal = document.getElementById("myModal");
 const btn = document.getElementById("openChat");
 const span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+//btn.onclick = function() {
+//  modal.style.display = "block";
+//}
 
 span.onclick = function() {
   modal.style.display = "none";
