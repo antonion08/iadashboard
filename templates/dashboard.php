86e26c147b5d6874fba6ae28aca8e7edb4f768c3
antonion08 <antonio08062004@gmail.com>
2024-10-23 14:24:38 -0400

    Ajustes en el nombre del estudiante en dashboard.html

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1,28 +1,30 @@
 <?php
-$servername = "sql3.freesqldatabase.com";
-$username = "sql3738152";
-$password = "PnV6EPU17z";
-$dbname = "sql3738152";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bd_edudash";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Consulta SQL para obtener el nombre del estudiante
-$sql = "SELECT nombre FROM estudiantes WHERE id = 1"; // Ajusta la consulta según tu estructura de tabla
+$sql = "SELECT nombre_datos_personales FROM datos_personales WHERE id_datos_personales = 1"; // Ajusta la consulta según tu estructura de tabla
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Obtener el nombre del primer resultado
     $row = $result->fetch_assoc();
-    $nombreEstudiante = $row["nombre"];
+    $nombreEstudiante = $row["nombre_datos_personales"];
 } else {
     echo "No se encontró ningún estudiante";
 }
 
-
-// Consulta para obtener la ruta de la imagen
+/*
+Consulta para obtener la ruta de la imagen
 $sql = "SELECT imagen FROM tu_tabla WHERE id = 1";
 $result = $conn->query($sql);
+*/
 
+/*
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     $rutaImagen = $row["imagen"];
@@ -32,4 +34,5 @@ if ($result->num_rows > 0) {
 } else {
     echo "No se encontró la imagen";
 }
+*/
 ?>
